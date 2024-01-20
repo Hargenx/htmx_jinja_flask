7b9ea6e438861ab87839902632ba20b023df2b21
Raphael <Hargenx@hotmail.com>
2024-01-19 22:17:16 -0300

    Erros

--- main.py
+++ main.py
@@ -2,29 +2,30 @@ import base64
 import json
 import zlib
 from flask import Flask, render_template, request
-import markupsafe
+from markupsafe import Markup
 
 app = Flask(__name__)
 
 @app.route("/", methods=['get', 'post'])
 def tarefas() -> str:
-    return render_template('index.html', valor = get_valor, salvar_valor = estado_salvo)
+    return render_template('index.html', valor=get_valor, salvar_valor=estado_salvo)
 
 def get_valor():
     if 'estado' in request.form:
-        return desceralizar(request.form['estado'])
+        return desserializar(request.form['estado'])
     return {}
 
 def estado_salvo(estado) -> str:
-    return markupsafe(f'<input type = "hidden" name = "estado" value = "{serializar(estado)}">')
+    return Markup(f'<input type="hidden" name="estado" value="{serializar(estado)}">')
+
 
 @app.template_filter()
-def desceralizar(valor) -> str:
-    return base64.b64encode(zlib.compress(json.dumps(valor).encode())).decode
+def desserializar(valor: str):
+    return json.loads(zlib.decompress(base64.b64decode(valor)))
 
 @app.template_filter()
-def serializar(valor: str):
-    return json.load(zlib.decompress(base64.b64decode(valor)))
+def serializar(valor) -> str:
+    return base64.b64encode(zlib.compress(json.dumps(valor).encode())).decode()
 
 if __name__ == "__main__":
     app.run(host='127.0.0.1', port=88, debug=True)
